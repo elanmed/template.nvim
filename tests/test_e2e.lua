87e365c7d27074e1c56dbb300a19fbb5fdf9ffe1
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-09-05 14:31:55 -0400

    init: tests

---
+++ tests/test_e2e.lua
@@ -0,0 +1,15 @@
+local new_set = MiniTest.new_set
+local child = MiniTest.new_child_neovim()
+
+local T = new_set {
+  hooks = {
+    pre_case = function()
+      child.restart { "-u", "scripts/minimal_init.lua" }
+      child.bo.readonly = false
+      child.lua [[require("template").setup()]]
+    end,
+    post_once = child.stop,
+  },
+}
+
+return T
